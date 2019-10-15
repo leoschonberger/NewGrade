c40aac5c48ae80271f5d82b291fe87222abd718e
Leo Schonberger <leoschonberger@gmail.com>
2019-10-15 11:30:24 -0700

    Changed Jsoup repo settings, gradebook page is now doc

--- GRABBIE GRABIIE CODE PARSE/src/GradeBookParse.java
+++ GRABBIE GRABIIE CODE PARSE/src/GradeBookParse.java
@@ -1,18 +1,17 @@
 import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-
 import java.io.IOException;
 
 public class GradeBookParse {
     //Uses GradeBook URL and cookies from loginForm to open the GradesPage
-    public static String ConnectToGradesPage(Connection.Response loginForm, String GradeBookUrl) throws IOException {
+    public static Document ConnectToGradesPage(Connection.Response loginForm, String GradeBookUrl) throws IOException {
         ParseGradebookUrl parseGradebookUrl = new ParseGradebookUrl(GradeBookUrl);
         Document doc = Jsoup.connect(parseGradebookUrl.createGradeBookUrl())
                 .cookies(loginForm.cookies())
                 .post(); //logs in
 
-        String GradeBookPage = doc.toString();
+        Document GradeBookPage = doc;
         System.out.println(GradeBookPage);
         return GradeBookPage;
     }
